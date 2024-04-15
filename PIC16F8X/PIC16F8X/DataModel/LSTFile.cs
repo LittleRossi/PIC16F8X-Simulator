97357109f6a5d6e150130b46cab5357bca4f6b05
Nick Starzmann <nick@starzmann.net>
2024-04-15 15:11:52 +0200

    added(LSTFile): added ObservableObject to be able to send notifications when a property is changed

--- PIC16F8X/PIC16F8X/DataModel/LSTFile.cs
+++ PIC16F8X/PIC16F8X/DataModel/LSTFile.cs
@@ -1,10 +1,15 @@
-﻿namespace PIC16F8X.DataModel
+﻿using System.Collections.ObjectModel;
+using System.DirectoryServices.ActiveDirectory;
+
+namespace PIC16F8X.DataModel
 {
     class LSTFile
     {
+        readonly ObservableCollection<SourceLine> sourceLines = new ObservableCollection<SourceLine>();
 
         public LSTFile(string path)
         {
+            
             // Neues LSTFile wird erzeugt, wenn LST Datei in UI ausgewählt wird
             // Dadurch wird dieses dann direkt gesetzt in Programmspeicher und initialisiert
 
@@ -15,5 +20,10 @@
             // prüfen nach hexadecimal Command => In liste von String
             // Aus String liste der Commands SetProgramm() aufrufen und dort die Liste übergeben um das Programm zu initialisieren
         }
+    }
+
+    class SourceLine : ObservableObject
+    {
+
     }
 }
---
+++ PIC16F8X/PIC16F8X/DataModel/ObservableObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace PIC16F8X.DataModel
+{
+
+    // ##########################################################################################
+    // # We need an Observable object type as SourceLine, because we need to throw an event,    #
+    // # when a new breakoint is set in the UI. Otherwise we can´t recognize a change in the    #
+    // # programm while running.                                                                #
+    // ##########################################################################################
+
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // method to set the new Value and send a notification, that a value has changed
+        protected virtual void SetAndNotify<T>(ref T field, T value, Expression<Func<T>> property)
+        {
+            if (!object.ReferenceEquals(field, value))
+            {
+                field = value;
+                this.OnPropertyChanged(property);
+            }
+        }
+
+        // method to create a new event with the changedProperty as name
+        protected virtual void OnPropertyChanged<T>(Expression<Func<T>> changedProperty)
+        {
+            if (PropertyChanged != null)
+            {
+                string name = ((MemberExpression)changedProperty.Body).Member.Name;
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
